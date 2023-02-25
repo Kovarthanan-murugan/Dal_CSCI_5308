82ca560a0d23b917a74ddbe5dffa83aaecec755f
Kovarthanan-murugan <kovarthan.ece@gmail.com>
2023-02-25 00:37:54 -0400

    Add functions to do zigzag level order traversal

--- src/main/java/org/zigZagTraversalProblem/zigZagTraversal.java
+++ src/main/java/org/zigZagTraversalProblem/zigZagTraversal.java
@@ -1,5 +1,7 @@
 package org.zigZagTraversalProblem;
 public class zigZagTraversal {
+    int[] travedFinal;
+    int travedCount =0;
     public static zigZagNodes createBinaryTree(int[] values, int index) {
         if (index >= values.length || values[index] == -1) {
             return null;
@@ -9,6 +11,23 @@ public class zigZagTraversal {
         node.left = createBinaryTree(values, 2 * index + 1);
         node.right = createBinaryTree(values, 2 * index + 2);
         return node;
+    }
+    public int[]  zigzagLevelOrder(zigZagNodes root,int count) {
+        System.out.println("inside zizag");
+        if (root == null) {
+            return null;
+        }
+        travedFinal = new int[count];
+
+        boolean isZigzag = false;
+
+        int height = getHeight(root);
+
+        for (int level = 1; level <= height; level++) {
+            printLevel(root, level, isZigzag);
+            isZigzag = !isZigzag;
+        }
+        return travedFinal;
     }
 
     public int getHeight(zigZagNodes node) {
@@ -23,6 +42,25 @@ public class zigZagTraversal {
         System.out.println("rightHeight-->"+rightHeight);
 
         return Math.max(leftHeight,rightHeight)+1;
+    }
+    private void printLevel(zigZagNodes node, int level, boolean isZigzag) {
+        if (node == null) {
+            return;
+        }
+
+        if (level == 1) {
+            System.out.print(node.val + " ");
+            travedFinal[travedCount] = node.val;
+            travedCount++;
+        } else if (level > 1) {
+            if (isZigzag) {
+                printLevel(node.right, level - 1, isZigzag);
+                printLevel(node.left, level - 1, isZigzag);
+            } else {
+                printLevel(node.left, level - 1, isZigzag);
+                printLevel(node.right, level - 1, isZigzag);
+            }
+        }
     }
 
 
