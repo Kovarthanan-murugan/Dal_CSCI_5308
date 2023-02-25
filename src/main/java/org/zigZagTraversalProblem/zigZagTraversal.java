589a90112ff03222840b1e22c5c9e1699b46a1b3
Kovarthanan-murugan <kovarthan.ece@gmail.com>
2023-02-24 22:58:52 -0400

    Add funtion to return height of the tree

--- src/main/java/org/zigZagTraversalProblem/zigZagTraversal.java
+++ src/main/java/org/zigZagTraversalProblem/zigZagTraversal.java
@@ -6,10 +6,24 @@ public class zigZagTraversal {
         }
 
         zigZagNodes node = new zigZagNodes(values[index]);
-
+        node.left = createBinaryTree(values, 2 * index + 1);
+        node.right = createBinaryTree(values, 2 * index + 2);
         return node;
     }
 
+    public int getHeight(zigZagNodes node) {
+        System.out.println("inside height");
+        if (node == null) {
+            return 0;
+        }
+        System.out.println("beforeleft");
+        int leftHeight = getHeight(node.left);
+        System.out.println("leftHeight-->"+leftHeight);
+        int rightHeight = getHeight(node.right);
+        System.out.println("rightHeight-->"+rightHeight);
+
+        return Math.max(leftHeight,rightHeight)+1;
+    }
 
 
     public static void main(String[] args) {
